01242e31f678f45617cc57e8764fe5433ccbb219
Simen Bekkhus <sbekkhus91@gmail.com>
2016-03-05 13:36:21 +0100

    Use ava

--- test.js
+++ test.js
@@ -1,133 +1,126 @@
-/* eslint-env mocha */
-
-import assert from 'assert'
+import test from 'ava'
 import sinon from 'sinon'
+import 'babel-register'
 
 import waitUntilPromise, { setPromiseImplementation } from './waitUntilPromise'
 
-describe('wait-until-promise', () => {
-  beforeEach(() => {
-    setPromiseImplementation(Promise)
-  })
+test.beforeEach(() => {
+  setPromiseImplementation(Promise)
+})
 
-  it('resolve if function returns true', () => {
-    return waitUntilPromise(() => true)
-  })
+test('resolve if function returns true', () => {
+  return waitUntilPromise(() => true)
+})
 
-  it('resolve with the return value', () => {
-    return waitUntilPromise(() => 'this is a truthy value')
-      .then((returnValue) => assert.equal(returnValue, 'this is a truthy value'))
-  })
+test('resolve with the return value', (t) => {
+  return waitUntilPromise(() => 'this is a truthy value')
+    .then((returnValue) => t.ok(returnValue, 'this is a truthy value'))
+})
 
-  it('reject if function returns false', (done) => {
-    waitUntilPromise(() => false)
-      .then(() => done(new Error('it should reject')))
-      .catch((err) => {
-        assert.equal(err, 'Wait until promise timed out')
+test('reject if function returns false', (t) => {
+  t.plan(1)
 
-        done()
-      })
-  })
-
-  it('should allow setting custom Promise implementation', () => {
-    const spy = sinon.spy()
-
-    setPromiseImplementation(spy)
-    waitUntilPromise()
-
-    assert.ok(spy.calledWithNew)
-  })
-
-  it('should allow setting custom maxWait', () => {
-    let count = 0
-    sinon.spy(global, 'setTimeout')
-    sinon.spy(global, 'setInterval')
-
-    return waitUntilPromise(() => count++ > 0, 32)
-      .then(() => {
-        assert.ok(global.setTimeout.calledOnce)
-        assert.ok(global.setInterval.calledOnce)
-
-        assert.equal(global.setTimeout.getCall(0).args[1], 32)
-      })
-      .then(() => {
-        global.setTimeout.restore()
-        global.setInterval.restore()
-      })
-  })
-
-  it('should allow setting custom checkDelay', () => {
-    let count = 0
-    sinon.spy(global, 'setTimeout')
-    sinon.spy(global, 'setInterval')
-
-    return waitUntilPromise(() => count++ > 0, undefined, 32)
-      .then(() => {
-        assert.ok(global.setTimeout.calledOnce)
-        assert.ok(global.setInterval.calledOnce)
-
-        assert.equal(global.setInterval.getCall(0).args[1], 32)
-      })
-      .then(() => {
-        global.setTimeout.restore()
-        global.setInterval.restore()
-      })
-  })
-
-  it('should reject with the exception if the functions throws', (done) => {
-    waitUntilPromise(() => ({}).someFunction())
-      .then(() => done(new Error('it should reject')))
-      .catch((err) => {
-        assert.ok(/is not a function/.test(err.message))
-
-        done()
-      })
-  })
-
-  it('should not call setTimeout or setInterval if function immediately returns truthy', () => {
-    sinon.spy(global, 'setTimeout')
-    sinon.spy(global, 'setInterval')
-
-    return waitUntilPromise(() => true)
-      .then(() => {
-        sinon.assert.notCalled(global.setTimeout)
-        sinon.assert.notCalled(global.setInterval)
-      })
-      .then(() => {
-        global.setTimeout.restore()
-        global.setInterval.restore()
-      })
-  })
-
-  it('should call setTimeout or setInterval once if function returns truthy', () => {
-    let count = 0
-    sinon.spy(global, 'setTimeout')
-    sinon.spy(global, 'setInterval')
-
-    return waitUntilPromise(() => count++ > 0)
-      .then(() => {
-        sinon.assert.callCount(global.setTimeout, 1)
-        sinon.assert.callCount(global.setInterval, 1)
-      })
-      .then(() => {
-        global.setTimeout.restore()
-        global.setInterval.restore()
-      })
-  })
-
-  it('should reject in timer if function throws', (done) => {
-    let count = 0
-
-    return waitUntilPromise(() => {
-      if (count++ === 0) return false;
-
-      ({}).someFunction()
+  return waitUntilPromise(() => false)
+    .catch((err) => {
+      t.is(err, 'Wait until promise timed out')
     })
-      .then(() => done(new Error('it should reject')))
-      .catch((err) => {
-        assert.ok(/is not a function/.test(err.message))
+})
 
-        done()
-      })
+test('should allow setting custom Promise implementation', (t) => {
+  const spy = sinon.spy()
+
+  setPromiseImplementation(spy)
+  waitUntilPromise()
+
+  t.ok(spy.calledWithNew)
+})
+
+test.serial('should allow setting custom maxWait', (t) => {
+  let count = 0
+  sinon.spy(global, 'setTimeout')
+  sinon.spy(global, 'setInterval')
+
+  return waitUntilPromise(() => count++ > 0, 32)
+    .then(() => {
+      t.ok(global.setTimeout.calledOnce)
+      t.ok(global.setInterval.calledOnce)
+
+      t.is(global.setTimeout.getCall(0).args[1], 32)
+    })
+    .then(() => {
+      global.setTimeout.restore()
+      global.setInterval.restore()
+    })
+})
+
+test.serial('should allow setting custom checkDelay', (t) => {
+  let count = 0
+  sinon.spy(global, 'setTimeout')
+  sinon.spy(global, 'setInterval')
+
+  return waitUntilPromise(() => count++ > 0, undefined, 32)
+    .then(() => {
+      t.ok(global.setTimeout.calledOnce)
+      t.ok(global.setInterval.calledOnce)
+
+      t.is(global.setInterval.getCall(0).args[1], 32)
+    })
+    .then(() => {
+      global.setTimeout.restore()
+      global.setInterval.restore()
+    })
+})
+
+test('should reject with the exception if the functions throws', (t) => {
+  t.plan(1)
+
+  return waitUntilPromise(() => ({}).someFunction())
+    .catch((err) => {
+      t.ok(/is not a function/.test(err.message))
+    })
+})
+
+test.serial('should not call setTimeout or setInterval if function immediately returns truthy', () => {
+  sinon.spy(global, 'setTimeout')
+  sinon.spy(global, 'setInterval')
+
+  return waitUntilPromise(() => true)
+    .then(() => {
+      sinon.assert.notCalled(global.setTimeout)
+      sinon.assert.notCalled(global.setInterval)
+    })
+    .then(() => {
+      global.setTimeout.restore()
+      global.setInterval.restore()
+    })
+})
+
+test.serial('should call setTimeout or setInterval once if function returns truthy', () => {
+  let count = 0
+  sinon.spy(global, 'setTimeout')
+  sinon.spy(global, 'setInterval')
+
+  return waitUntilPromise(() => count++ > 0)
+    .then(() => {
+      sinon.assert.callCount(global.setTimeout, 1)
+      sinon.assert.callCount(global.setInterval, 1)
+    })
+    .then(() => {
+      global.setTimeout.restore()
+      global.setInterval.restore()
+    })
+})
+
+test('should reject in timer if function throws', (t) => {
+  t.plan(1)
+  let count = 0
+
+  return waitUntilPromise(() => {
+    if (count++ === 0) return false;
+
+    ({}).someFunction()
   })
+    .catch((err) => {
+      t.ok(/is not a function/.test(err.message))
+    })
 })
