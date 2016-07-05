64ca2e016029c9b0c091f4fff2ac1c15509f6097
Simen Bekkhus <sbekkhus91@gmail.com>
2016-07-05 23:06:47 +0200

    Use eslint-config-airbnb-base instead of standard

--- test.js
+++ test.js
@@ -1,129 +1,129 @@
-import test from 'ava'
-import sinon from 'sinon'
-import BluebirdPromise from 'bluebird'
-import 'babel-register'
-import waitUntilPromise, { setPromiseImplementation } from './waitUntilPromise'
+/* eslint import/no-extraneous-dependencies: ["error", { "devDependencies": true }] */
+
+import test from 'ava';
+import sinon from 'sinon';
+import BluebirdPromise from 'bluebird';
+import 'babel-register';
+import waitUntilPromise, { setPromiseImplementation } from './waitUntilPromise';
 
 test.beforeEach('before', () => {
-  setPromiseImplementation(Promise)
-})
+  setPromiseImplementation(Promise);
+});
 
-test('resolve if function returns true', () => {
-  return waitUntilPromise(() => true)
-})
+test('resolve if function returns true', () => waitUntilPromise(() => true));
 
 test('resolve with the return value', async t => {
   t.truthy(
     await waitUntilPromise(() => 'this is a truthy value'),
     'this is a truthy value'
-  )
-})
+  );
+});
 
 test('reject if function returns false', t => {
-  t.throws(waitUntilPromise(() => false), 'Wait until promise timed out')
-})
+  t.throws(waitUntilPromise(() => false), 'Wait until promise timed out');
+});
 
 test('should allow setting custom Promise implementation', t => {
-  const resolve = sinon.spy()
+  const resolve = sinon.spy();
 
-  setPromiseImplementation({ resolve })
+  setPromiseImplementation({ resolve });
 
-  waitUntilPromise(() => true)
+  waitUntilPromise(() => true);
 
-  t.truthy(resolve.called)
-})
+  t.truthy(resolve.called);
+});
 
 test.serial('should allow setting custom maxWait', async t => {
-  let count = 0
-  sinon.spy(global, 'setTimeout')
-  sinon.spy(global, 'setInterval')
+  let count = 0;
+  sinon.spy(global, 'setTimeout');
+  sinon.spy(global, 'setInterval');
 
-  await waitUntilPromise(() => count++ > 0, 32)
+  await waitUntilPromise(() => count++ > 0, 32);
 
-  t.truthy(global.setTimeout.calledOnce)
-  t.truthy(global.setInterval.calledOnce)
+  t.truthy(global.setTimeout.calledOnce);
+  t.truthy(global.setInterval.calledOnce);
 
-  t.truthy(global.setTimeout.getCall(0).args[1], 32)
+  t.truthy(global.setTimeout.getCall(0).args[1], 32);
 
-  global.setTimeout.restore()
-  global.setInterval.restore()
-})
+  global.setTimeout.restore();
+  global.setInterval.restore();
+});
 
 test.serial('should allow setting custom checkDelay', async t => {
-  let count = 0
-  sinon.spy(global, 'setTimeout')
-  sinon.spy(global, 'setInterval')
+  let count = 0;
+  sinon.spy(global, 'setTimeout');
+  sinon.spy(global, 'setInterval');
 
-  await waitUntilPromise(() => count++ > 0, undefined, 32)
+  await waitUntilPromise(() => count++ > 0, undefined, 32);
 
-  t.truthy(global.setTimeout.calledOnce)
-  t.truthy(global.setInterval.calledOnce)
+  t.truthy(global.setTimeout.calledOnce);
+  t.truthy(global.setInterval.calledOnce);
 
-  t.truthy(global.setInterval.getCall(0).args[1], 32)
+  t.truthy(global.setInterval.getCall(0).args[1], 32);
 
-  global.setTimeout.restore()
-  global.setInterval.restore()
-})
+  global.setTimeout.restore();
+  global.setInterval.restore();
+});
 
 test('should reject with the exception if the functions throws', t => {
-  t.throws(waitUntilPromise(() => ({}).someFunction()), /is not a function/)
-})
+  t.throws(waitUntilPromise(() => ({}).someFunction()), /is not a function/);
+});
 
 test.serial('should not call setTimeout or setInterval if function immediately returns truthy', async () => {
-  sinon.spy(global, 'setTimeout')
-  sinon.spy(global, 'setInterval')
+  sinon.spy(global, 'setTimeout');
+  sinon.spy(global, 'setInterval');
 
-  await waitUntilPromise(() => true)
+  await waitUntilPromise(() => true);
 
-  sinon.assert.notCalled(global.setTimeout)
-  sinon.assert.notCalled(global.setInterval)
+  sinon.assert.notCalled(global.setTimeout);
+  sinon.assert.notCalled(global.setInterval);
 
-  global.setTimeout.restore()
-  global.setInterval.restore()
-})
+  global.setTimeout.restore();
+  global.setInterval.restore();
+});
 
 test.serial('should call setTimeout or setInterval once if function returns truthy', async () => {
-  let count = 0
+  let count = 0;
 
-  sinon.spy(global, 'setTimeout')
-  sinon.spy(global, 'setInterval')
+  sinon.spy(global, 'setTimeout');
+  sinon.spy(global, 'setInterval');
 
-  await waitUntilPromise(() => count++ > 0)
-  sinon.assert.callCount(global.setTimeout, 1)
-  sinon.assert.callCount(global.setInterval, 1)
+  await waitUntilPromise(() => count++ > 0);
+  sinon.assert.callCount(global.setTimeout, 1);
+  sinon.assert.callCount(global.setInterval, 1);
 
-  global.setTimeout.restore()
-  global.setInterval.restore()
-})
+  global.setTimeout.restore();
+  global.setInterval.restore();
+});
 
 test('should reject in timer if function throws', t => {
-  let count = 0
+  let count = 0;
 
   const prom = waitUntilPromise(() => {
     if (count++ === 0) {
-      return false
+      return;
     }
 
-    ({}).someFunction()
-  })
+    ({}).someFunction();
+  });
 
-  t.throws(prom, /is not a function/)
-})
+  t.throws(prom, /is not a function/);
+});
 
 test.serial('should throw if no Promise is available', t => {
-  setPromiseImplementation(null)
+  setPromiseImplementation(null);
 
-  t.throws(() => waitUntilPromise(() => true), /Wait Until Promise: No global Promise available/)
-})
+  t.throws(() => waitUntilPromise(() => true), /Wait Until Promise: No global Promise available/);
+});
 
 test.serial('should reject with TimeoutError if available', async t => {
-  t.plan(1)
+  t.plan(1);
 
-  setPromiseImplementation(BluebirdPromise)
+  setPromiseImplementation(BluebirdPromise);
 
   try {
-    await waitUntilPromise(() => false)
+    await waitUntilPromise(() => false);
   } catch (e) {
-    t.truthy(e instanceof BluebirdPromise.TimeoutError)
+    t.truthy(e instanceof BluebirdPromise.TimeoutError);
   }
-})
+});
--- waitUntilPromise.js
+++ waitUntilPromise.js
@@ -1,8 +1,9 @@
-let PromiseImplementation
+let PromiseImplementation;
 
 try {
-  PromiseImplementation = Promise
+  PromiseImplementation = Promise;
 } catch (err) {
+  // ignored
 }
 
 /**
@@ -12,9 +13,9 @@ try {
  * @param {number} interval - Id of the interval to clear.
  * @private
  */
-function clearTimers (timeout, interval) {
-  clearTimeout(timeout)
-  clearInterval(interval)
+function clearTimers(timeout, interval) {
+  clearTimeout(timeout);
+  clearInterval(interval);
 }
 
 /**
@@ -24,8 +25,8 @@ function clearTimers (timeout, interval) {
  * @static
  */
 export const setPromiseImplementation = implementation => {
-  PromiseImplementation = implementation
-}
+  PromiseImplementation = implementation;
+};
 
 /**
  * Create a {@link Promise} that resolves if the given escapeFunction returns a truthy value, and rejects if it throws
@@ -40,46 +41,46 @@ export const setPromiseImplementation = implementation => {
  */
 export default (escapeFunction, maxWait = 50, checkDelay = 1) => {
   if (PromiseImplementation == null) {
-    throw new Error('Wait Until Promise: No global Promise available, make sure to use `setPromiseImplementation`.')
+    throw new Error('Wait Until Promise: No global Promise available, make sure to use `setPromiseImplementation`.');
   }
 
   // Run the function once without setting up any listeners in case it's already true
   try {
-    const escapeFunctionRes = escapeFunction()
+    const escapeFunctionRes = escapeFunction();
 
-    if (escapeFunctionRes) return PromiseImplementation.resolve(escapeFunctionRes)
+    if (escapeFunctionRes) return PromiseImplementation.resolve(escapeFunctionRes);
   } catch (e) {
-    return PromiseImplementation.reject(e)
+    return PromiseImplementation.reject(e);
   }
 
   return new PromiseImplementation((resolve, reject) => {
-    let maxWaitTimeout
+    let maxWaitTimeout;
 
     const interval = setInterval(() => {
       try {
-        const escapeFunctionRes = escapeFunction()
+        const escapeFunctionRes = escapeFunction();
 
         if (escapeFunctionRes) {
-          clearTimers(maxWaitTimeout, interval)
+          clearTimers(maxWaitTimeout, interval);
 
-          resolve(escapeFunctionRes)
+          resolve(escapeFunctionRes);
         }
       } catch (e) {
-        clearTimers(maxWaitTimeout, interval)
+        clearTimers(maxWaitTimeout, interval);
 
-        reject(e)
+        reject(e);
       }
-    }, checkDelay)
+    }, checkDelay);
 
     maxWaitTimeout = setTimeout(() => {
-      clearTimers(maxWaitTimeout, interval)
+      clearTimers(maxWaitTimeout, interval);
 
       // Try to reject with a TimeoutError, like Bluebird has
       if (PromiseImplementation.TimeoutError) {
-        reject(new PromiseImplementation.TimeoutError('Wait until promise timed out'))
+        reject(new PromiseImplementation.TimeoutError('Wait until promise timed out'));
       } else {
-        reject(new Error('Wait until promise timed out'))
+        reject(new Error('Wait until promise timed out'));
       }
-    }, maxWait)
-  })
-}
+    }, maxWait);
+  });
+};
